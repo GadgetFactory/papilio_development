69a69097a7115df1e0ffc26d05357dd81f15090e
Jack Gassett <jack.gassett@gadgetfactory.net>
2025-12-06 16:20:58 -0700

    Integrate papilio_audio MOD player env: map example in examples_select, add audio_mod_player PlatformIO env; submodule updated to include AudioOutputWishbone (direct write) and passthrough example/gateware. Current state: audio plays but stretched/fuzzy; further cadence/FIFO work pending.

--- src/utilities/examples_select.py
+++ src/utilities/examples_select.py
@@ -27,6 +27,8 @@ ENV_MAP = {
     'ym2149_demo': 'libs/papilio_audio/examples/ym2149_demo/ym2149_demo.ino',
     'ym_player_demo': 'libs/papilio_audio/examples/ym_player_demo/ym_player_demo.ino',
     'pokey_demo': 'libs/papilio_audio/examples/pokey_demo/pokey_demo.ino',
+    'audio_pass_demo': 'libs/papilio_audio/examples/audio_pass_demo/audio_pass_demo.ino',
+    'audio_mod_player': 'libs/papilio_audio/examples/audio_mod_player/audio_mod_player.ino',
 }
 
 # __file__ may not be defined in PlatformIO pre scripts; fallback to CWD
